e7539c329fbe7a95892dda9923083a2c8c36d500
yunubuntu <744561873@qq.com>
2020-10-09 23:32:54 +0800

    mybatis 多对多 init

---
+++ mybatie/mybatis_day03_03many/src/main/java/com/itcast/dao/IRoleDao.java
@@ -0,0 +1,14 @@
+package com.itcast.dao;
+
+import com.itcast.domain.Role;
+
+import java.util.List;
+
+public interface IRoleDao {
+
+    /**
+     * 查询所有角色
+     * @return
+     */
+    List<Role> findAll();
+}
